8139f872a23ceabef883e7a443a2f81a7280c768
Ali Kaya <alikaya4275@gmail.com>
2026-02-08 11:12:27 +0300

    feat: add Product entity to Entity layer

---
+++ ProductFlowServer/ProductFlowServer.Entities/Concrete/Product.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProductFlowServer.Entities.Concrete
+{
+    public sealed class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string ProductCode { get; set; }
+        public decimal Price { get; set; }
+        public int Quantity { get; set; }
+        public bool IsActive { get; set; }
+        public DateTime? CreateDate { get; set; }
+        public DateTime? UpdateDate { get; set; }
+    }
+}
