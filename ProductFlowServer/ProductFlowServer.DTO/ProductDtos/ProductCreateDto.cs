f8b2b8ef7a2e8eb535eabae4e61f9560e93daea1
Ali Kaya <alikaya4275@gmail.com>
2026-02-08 11:16:53 +0300

    feat: add ProductListDTOs & ProductCreateDTOs for data transfer

---
+++ ProductFlowServer/ProductFlowServer.DTO/ProductDtos/ProductCreateDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProductFlowServer.DTO.ProductDtos
+{
+    public sealed class ProductCreateDto
+    {
+        public string Name { get; set; }
+        public string ProductCode { get; set; }
+        public decimal Price { get; set; }
+        public int StockQuantity { get; set; }
+    }
+}
---
+++ ProductFlowServer/ProductFlowServer.DTO/ProductDtos/ProductListDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProductFlowServer.DTO.ProductDtos
+{
+    public sealed class ProductListDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string ProductCode { get; set; }
+        public decimal Price { get; set; }
+        public int Quantity { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
