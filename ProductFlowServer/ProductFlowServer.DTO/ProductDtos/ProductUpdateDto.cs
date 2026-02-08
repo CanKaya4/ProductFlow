9d291797ce846825ad5b84f13e93a554539408be
Ali Kaya <alikaya4275@gmail.com>
2026-02-08 12:51:52 +0300

    fix(dto): add Id property to ProductUpdateDto

--- ProductFlowServer/ProductFlowServer.DTO/ProductDtos/ProductUpdateDto.cs
+++ ProductFlowServer/ProductFlowServer.DTO/ProductDtos/ProductUpdateDto.cs
@@ -6,6 +6,7 @@ namespace ProductFlowServer.DTO.ProductDtos
 {
     public sealed class ProductUpdateDto
     {
+        public int Id { get; set; }
         public string Name { get; set; }
         public string ProductCode { get; set; }
         public decimal Price { get; set; }
