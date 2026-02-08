528275bcb63a26bba989e2a639f26cbfbe8f3d47
Ali Kaya <alikaya4275@gmail.com>
2026-02-08 14:35:01 +0300

    fix(entity): rename StockQuantity to Quantity in ProductDtos

--- ProductFlowServer/ProductFlowServer.DTO/ProductDtos/ProductCreateDto.cs
+++ ProductFlowServer/ProductFlowServer.DTO/ProductDtos/ProductCreateDto.cs
@@ -9,6 +9,6 @@ namespace ProductFlowServer.DTO.ProductDtos
         public string Name { get; set; }
         public string ProductCode { get; set; }
         public decimal Price { get; set; }
-        public int StockQuantity { get; set; }
+        public int Quantity { get; set; }
     }
 }
