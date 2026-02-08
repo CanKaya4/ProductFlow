a4761d4aee9b62c1e98ec93de5e1448a04844e55
Ali Kaya <alikaya4275@gmail.com>
2026-02-08 12:50:16 +0300

    feat(api): implement ProductsController with full CRUD support

---
+++ ProductFlowServer/ProductFlowServer.API/Controllers/ProductsController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ProductFlowServer.Business.Abstract;
+using ProductFlowServer.DTO.ProductDtos;
+
+namespace ProductFlowServer.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly IProductService _productService;
+
+        public ProductsController(IProductService productService)
+        {
+            _productService = productService;
+        }
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var products = await _productService.GetAllAsync();
+            return Ok(products);
+        }
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(int id)
+        {
+            var product = await _productService.GetByIdAsync(id);
+            if (product == null)
+            {
+                return NotFound($"{id} numaralı ürün bulunamadı");
+            }
+            return Ok(product);
+        }
+        [HttpPost]
+        public async Task<IActionResult> Create(ProductCreateDto productCreateDto)
+        {
+            await _productService.AddAsync(productCreateDto);
+            return StatusCode(201);
+        }
+        [HttpPut]
+        public async Task<IActionResult> Update(ProductUpdateDto productUpdateDto)
+        {
+            await _productService.UpdateAsync(productUpdateDto);
+            return NoContent();
+        }
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            await _productService.DeleteAsync(id);
+            return NoContent();
+        }
+
+    }
+}
