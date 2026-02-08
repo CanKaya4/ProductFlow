1bdc7e58606b3ee829a8e9c7aa4740361f387ffd
Ali Kaya <alikaya4275@gmail.com>
2026-02-08 12:38:47 +0300

    feat(api): setup swagger documentation and middleware

--- ProductFlowServer/ProductFlowServer.API/Program.cs
+++ ProductFlowServer/ProductFlowServer.API/Program.cs
@@ -1,5 +1,6 @@
-using ProductFlowServer.DataAccess;
+using Microsoft.OpenApi;
 using ProductFlowServer.Business;
+using ProductFlowServer.DataAccess;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -9,13 +10,27 @@ builder.Services.AddDataAccessServices(builder.Configuration);
 builder.Services.AddBusinessServices(builder.Configuration);
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c =>
+{
 
+    c.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "ProductFlowServer API",
+        Version = "v1.0.0",
+        Description = "FullStack Developer - 1. Aþama Task"
+    });
+});
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "ProductFlow API v1");
+    });
 }
 
 app.UseHttpsRedirection();
